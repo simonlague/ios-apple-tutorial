6c16895fba783d490f9f507d18481f7710172871
Simon Laguë <simonlague@gmail.com>
2022-06-11 12:32:16 -0400

    Initial commit

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Simon Laguë on 2022-06-10.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
